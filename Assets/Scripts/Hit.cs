3c8ba1719e00a69e03f9bc1b865be7d3330cfdea
Kevin Frans <kevin@bobthechicken.com>
2015-03-08 10:39:00 -0700

    sprites and basic coloring

---
+++ Assets/Scripts/Hit.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hit : MonoBehaviour {
+
+	public Color maincolor;
+
+	public Transform bg;
+	public Transform border;
+	public Transform approach;
+
+	private float lifetime = 3;
+
+	// Use this for initialization
+	void Start () {
+		bg.GetComponent<SpriteRenderer>().color = maincolor;
+		approach.GetComponent<SpriteRenderer>().color = maincolor;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		lifetime -= Time.deltaTime*2;
+		approach.localScale = new Vector3(lifetime+1,lifetime+1,lifetime+1);
+	}
+}
