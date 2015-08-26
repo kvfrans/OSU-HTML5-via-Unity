540958d0cf1f2553037bb16fc76680c278c4513e
Kevin Frans <kevin@bobthechicken.com>
2015-08-25 18:25:40 -0700

    Library

--- Assets/Scripts/Hit.cs
+++ Assets/Scripts/Hit.cs
@@ -8,18 +8,205 @@ public class Hit : MonoBehaviour {
 	public Transform bg;
 	public Transform border;
 	public Transform approach;
+	public Transform ballbg;
+
+	public Transform fakehit;
+	public Transform fakehit2;
+
+	public LoadBeatmap.Beat beat;
 
 	private float lifetime = 3;
 
+	public float speed = 10;
+  	public Vector2 direction = new Vector2(0, 0);
+
+  	private Vector2 movement;
+
+  	private bool dead = false;
+  	private float fadeout = 1;
+
+  	// publ
+
+  	private bool moving = false;
+  	private bool reverse = false;
+
 	// Use this for initialization
 	void Start () {
-		bg.GetComponent<SpriteRenderer>().color = maincolor;
-		approach.GetComponent<SpriteRenderer>().color = maincolor;
+		bg.GetComponent<SpriteRenderer>().color = new Color(0,0,0,0);
+		approach.GetComponent<SpriteRenderer>().color = new Color(0,0,0,0);
+		border.GetComponent<SpriteRenderer>().color = new Color(0,0,0,0);
+		fakehit2.Find("bg").GetComponent<SpriteRenderer>().color = new Color(0,0,0,0);
+		fakehit2.Find("border").GetComponent<SpriteRenderer>().color = new Color(0,0,0,0);
+		fakehit.Find("bg").GetComponent<SpriteRenderer>().color = new Color(0,0,0,0);
+		fakehit.Find("border").GetComponent<SpriteRenderer>().color = new Color(0,0,0,0);
+
+		if(beat.type == "slider")
+		{
+			GetComponent<LineRenderer>().enabled = true;
+			GetComponent<LineRenderer>().SetPosition(0,beat.location);
+			GetComponent<LineRenderer>().SetPosition(1,beat.path[beat.path.Count -1]);
+			fakehit.position = beat.path[beat.path.Count-1];
+			fakehit2.position = beat.location;
+		}
+
 	}
 
 	// Update is called once per frame
 	void Update () {
+
+		if(beat.type == "slider")
+		{
+			fakehit.position = beat.path[beat.path.Count-1];
+			fakehit2.position = beat.location;
+		}
+		// Debug.Log(beat.repeats);
+		// Debug.Log(reverse);
+
+		if(lifetime > 2f)
+		{
+			GetComponent<LineRenderer>().SetColors(new Color(maincolor.r,maincolor.b,maincolor.g,(3f-lifetime)/1f),new Color(maincolor.r,maincolor.b,maincolor.g,(3f-lifetime)/1f));
+			bg.GetComponent<SpriteRenderer>().color = new Color(maincolor.r,maincolor.b,maincolor.g,(3f-lifetime)/1f);
+			border.GetComponent<SpriteRenderer>().color = new Color(255,255,255,(3f-lifetime)/1f);
+			// approach.GetComponent<SpriteRenderer>().color = new Color(maincolor.r,maincolor.b,maincolor.g,(3f-lifetime)/1f);
+			fakehit2.Find("bg").GetComponent<SpriteRenderer>().color = new Color(255,255,255,(3f-lifetime)/1f);
+			fakehit2.Find("border").GetComponent<SpriteRenderer>().color = new Color(255,255,255,(3f-lifetime)/1f);
+			// fakehit.Find("bg").GetComponent<SpriteRenderer>().color = new Color(255,255,255,(3f-lifetime)/1f);
+			fakehit.Find("border").GetComponent<SpriteRenderer>().color = new Color(255,255,255,(3f-lifetime)/1f);
+
+		}
+		else
+		{
+			bg.GetComponent<SpriteRenderer>().color = maincolor;
+			if(border != null)
+				border.GetComponent<SpriteRenderer>().color = new Color(255,255,255,1);
+			if(approach != null)
+				approach.GetComponent<SpriteRenderer>().color = maincolor;
+		}
 		lifetime -= Time.deltaTime*2;
-		approach.localScale = new Vector3(lifetime+1,lifetime+1,lifetime+1);
+		if(approach != null)
+		{
+			approach.localScale = new Vector3(lifetime+1,lifetime+1,lifetime+1);
+		}
+		if(lifetime <= 0)
+		{
+			if(beat.type == "point")
+			{
+				GetComponent<Animator>().Play( Animator.StringToHash( "Hit" ) );
+				dead = true;
+			}
+			else if(!moving)
+			{
+				moving = true;
+				Destroy(approach.gameObject);
+				Destroy(border.gameObject);
+				bg.GetComponent<Animator>().Play( Animator.StringToHash( "Ball" ) );
+				ballbg.GetComponent<SpriteRenderer>().enabled = true;
+			}
+
+
+
+		}
+
+		if(moving)
+		{
+			Vector2 target = beat.path[beat.path.Count - 1];
+			// Debug.Log(target);
+
+			if(reverse)
+			{
+				target = beat.location;
+			}
+
+			direction = target - new Vector2(transform.position.x,transform.position.y);
+			if(direction.sqrMagnitude < 25)
+			{
+				if(beat.repeats > 1)
+				{
+					beat.repeats--;
+					reverse = !reverse;
+				}
+				else
+				{
+					dead = true;
+				}
+			}
+
+			direction.Normalize();
+
+
+		}
+
+		movement = new Vector2(direction.x * speed,direction.y * speed);
+
+
+		if(dead)
+		{
+
+			fadeout -= Time.deltaTime * 3;
+
+			GetComponent<LineRenderer>().SetColors(new Color(maincolor.r,maincolor.b,maincolor.g,fadeout),new Color(maincolor.r,maincolor.b,maincolor.g,fadeout));
+			bg.GetComponent<SpriteRenderer>().color = new Color(maincolor.r,maincolor.b,maincolor.g,fadeout);
+			if(border != null)
+				border.GetComponent<SpriteRenderer>().color = new Color(255,255,255,fadeout);
+			// approach.GetComponent<SpriteRenderer>().color = new Color(maincolor.r,maincolor.b,maincolor.g,(3f-lifetime)/1f);
+			fakehit2.Find("bg").GetComponent<SpriteRenderer>().color = new Color(255,255,255,fadeout);
+			fakehit2.Find("border").GetComponent<SpriteRenderer>().color = new Color(255,255,255,fadeout);
+			// fakehit.Find("bg").GetComponent<SpriteRenderer>().color = new Color(255,255,255,(3f-lifetime)/1f);
+			fakehit.Find("border").GetComponent<SpriteRenderer>().color = new Color(255,255,255,fadeout);
+
+			if(fadeout < 0)
+			{
+				Destroy(gameObject);
+			}
+
+			movement = new Vector2(0,0);
+		}
+
+
+		if(Input.GetMouseButton(0))
+			{
+				checkTouch(Input.mousePosition);
+			}
+
+
 	}
+
+	void FixedUpdate()
+  	{
+    // Apply movement to the rigidbody
+    	GetComponent<Rigidbody2D>().velocity = movement;
+  	}
+
+  	void checkTouch(Vector2 pos)
+	{
+	    Vector3 wp = Camera.main.ScreenToWorldPoint(pos);
+    	Vector2 touchPos = new Vector2(wp.x, wp.y);
+    	var hit = Physics2D.OverlapPoint(touchPos);
+    	if(hit)
+    	{
+    		if(beat.type == "point")
+			{
+				GetComponent<Animator>().Play( Animator.StringToHash( "Hit" ) );
+				dead = true;
+
+				float score = 0;
+				if(lifetime < 0.5)
+				{
+					score = 300;
+				}
+				else if(lifetime < 1.5)
+				{
+					score = 100;
+				}
+				else if(lifetime < 2.5)
+				{
+					score = 50;
+				}
+				Debug.Log(score);
+
+			}
+    	}
+	}
+
+
 }
---
+++ Assets/Scripts/LoadBeatmap.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LoadBeatmap : MonoBehaviour {
+
+	public string filelocation;
+	public string beatmapname;
+
+	private List<string> commands;
+
+	private List<Beat> beats;
+
+	public Transform pointbeat;
+
+	public Transform background;
+
+	// Use this for initialization
+	void Start () {
+
+		beats = new List<Beat>();
+
+		commands = GetComponent<ReadFile>().Load(filelocation+"/"+beatmapname);
+
+		int songindex = commands.FindIndex(x => x.StartsWith("AudioFilename"));
+		string songloc = commands[songindex].Substring(15);
+		string songraw = songloc.Split("."[0])[0];
+		Debug.Log(songraw);
+		GetComponent<AudioSource>().clip = Resources.Load<AudioClip>(filelocation+"/"+songraw) as AudioClip;
+		GetComponent<AudioSource>().Play();
+
+		int imageindex = commands.FindIndex(x => x.StartsWith("0,0,"));
+		string imageloc = commands[imageindex].Substring(5);
+		string imageraw = imageloc.Split("."[0])[0];
+		Debug.Log(imageraw);
+		Texture2D tex = Resources.Load<Texture2D>(filelocation+"/"+imageraw) as Texture2D;
+		background.GetComponent<SpriteRenderer>().sprite = Sprite.Create(tex,new Rect(0,0,tex.width,tex.height),new Vector2(0.5f,0.5f));
+		// background.GetComponent<Spr
+		// GetComponent<AudioSource>().Play();
+
+		int beatsindex = commands.FindIndex(x => x.StartsWith("[HitObjects]"));
+		for(int i = beatsindex+1; i < commands.Count; i++)
+		{
+			string line = commands[i];
+			string[] components = line.Split(',');
+			// Debug.Log(line);
+			string type = "point";
+			int repeats = 0;
+			List<Vector2> path = new List<Vector2>();
+			if(components.Length > 6)
+			{
+				type = "slider";
+				string typeandsliders = components[5];
+				string[] tasComponents = typeandsliders.Split('|');
+				repeats = int.Parse(components[6]);
+				for(int x = 1; x < tasComponents.Length; x++)
+				{
+					path.Add(new Vector2(float.Parse(tasComponents[x].Split(':')[0]),float.Parse(tasComponents[x].Split(':')[1])));
+				}
+			}
+			Beat beat = new Beat(new Vector2(int.Parse(components[0]),int.Parse(components[1])),type,path,int.Parse(components[2]),repeats);
+			beats.Add(beat);
+		}
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Beat currentbeat = beats[0];
+		if(Time.time*1000 >= currentbeat.time - 1500)
+		{
+			var localbeat = Instantiate(pointbeat);
+			localbeat.position = currentbeat.location;
+			localbeat.GetComponent<Hit>().beat = currentbeat;
+			beats.Remove(currentbeat);
+		}
+	}
+
+	public class Beat
+	{
+		public Vector2 location;
+		public string type;
+		public List<Vector2> path;
+		public int time;
+		public int repeats;
+
+	    public Beat(Vector2 loc, string typ, List<Vector2> pat, int tim, int rep)
+	    {
+	        location = loc;
+	        type = typ;
+	        path = pat;
+	        time = tim;
+	        repeats = rep;
+	    }
+	}
+}
